929c78645c8919d7b5ab82ce132f8a2d4b23d3ef
kpnishanth <nishanth.prabhakaran@gmail.com>
2024-03-03 21:08:16 -0500

    intial commit

--- src/main/java/MazePath.java
+++ src/main/java/MazePath.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MazePath {
+
+    public static void main(String[] args) {
+        System.out.println(MazePath.getMazePath(0,0,2,2));
+    }
+    public static  List<String>getMazePath(int sr, int sc, int er, int ec){
+        List<String>returnPath= new ArrayList<>();
+        List<String>hp=new ArrayList<>();
+        List<String>vp=new ArrayList<>();
+        if(sr==er && sc==ec){
+            List<String> objectArrayList = new ArrayList<>();
+             objectArrayList.add("");
+             return objectArrayList;
+        }
+        if(sr<er){
+      hp=getMazePath(sr+1,sc,er,ec);
+            for (String p:hp) {
+                returnPath.add("h"+p);
+            }
+        }
+        if(sc<ec){
+      vp=getMazePath(sr,sc+1,er,ec);
+            for (String p:vp) {
+                returnPath.add("v"+p);
+            }
+        }
+
+
+
+            return returnPath;
+    }
 }
